b7266ba793255b10f00a2e2f2d5760066a997f70
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-04-26 11:24:31 +0200

    update to new dev branch

--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -1,11 +1,11 @@
 # bin/bash
 
-rm amp-app-0.2.0.zip
-rm -rf amp-app-0.2.0
-wget https://github.com/Auden-Musulin-Papers/amp-app/archive/refs/heads/v0.2.0.zip
-unzip v0.2.0
-mv amp-app-0.2.0/*.* ./
-mv amp-app-0.2.0/xslt/ ./xslt
-mv amp-app-0.2.0/html/ ./html
-rm -rf amp-app-0.2.0
-rm v0.2.0.zip
+rm amp-app-dev.zip
+rm -rf amp-app-dev
+wget https://github.com/Auden-Musulin-Papers/amp-app/archive/refs/heads/dev.zip
+unzip dev
+mv amp-app-dev/*.* ./
+mv amp-app-dev/xslt/ ./xslt
+mv amp-app-dev/html/ ./html
+rm -rf amp-app-dev
+rm dev.zip

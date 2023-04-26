e4fb7a14c11d7e8c96a39270fa1d4552f51b6712
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-04-26 11:29:45 +0200

    update dev workflow

--- fetch_ampdata_entities.sh
+++ fetch_ampdata_entities.sh
@@ -1,11 +1,11 @@
 # bin/bash
 
-rm entities.zip
+rm dev.zip
 rm -rf ./data
-wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/entities.zip
-unzip entities
-mv amp-data-entities/data ./data
-rm -rf amp-data-entities
-rm entities.zip
+wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/dev.zip
+unzip dev
+mv amp-data-dev/data ./data
+rm -rf amp-data-dev
+rm dev.zip
 
 rm -rf ./data/tmp

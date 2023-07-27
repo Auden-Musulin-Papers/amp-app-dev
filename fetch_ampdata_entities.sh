371716af637ef3d81e273779eaf958ef965823ef
Daniel_Stoxreiter <elsner.daniel@outlook.com>
2023-07-27 10:13:38 +0200

    changed data to main and reversed app to dev

--- fetch_ampdata_entities.sh
+++ fetch_ampdata_entities.sh
@@ -1,11 +1,11 @@
 # bin/bash
 
-rm dev04.zip
+rm main.zip
 rm -rf ./data
-wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/dev04.zip
-unzip dev04
-mv amp-data-dev04/data ./data
-rm -rf amp-data-dev04
-rm dev04.zip
+wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/main.zip
+unzip main
+mv amp-data-main/data ./data
+rm -rf amp-data-main
+rm main.zip
 
 rm -rf ./data/tmp
--- fetch_data_app.sh
+++ fetch_data_app.sh
@@ -1,12 +1,12 @@
 # bin/bash
 
-rm amp-app-main.zip
+rm amp-app-dev.zip
 rm -rf amp-app-dev
-wget https://github.com/Auden-Musulin-Papers/amp-app/archive/refs/heads/main.zip
-unzip main
-mv amp-app-main/*.* ./
-mv amp-app-main/xslt/ ./xslt
-mv amp-app-main/html/ ./html
-mv amp-app-main/build_app/ ./build_app
-rm -rf amp-app-main
-rm main.zip
+wget https://github.com/Auden-Musulin-Papers/amp-app/archive/refs/heads/dev.zip
+unzip dev
+mv amp-app-dev/*.* ./
+mv amp-app-dev/xslt/ ./xslt
+mv amp-app-dev/html/ ./html
+mv amp-app-dev/build_app/ ./build_app
+rm -rf amp-app-dev
+rm dev.zip
